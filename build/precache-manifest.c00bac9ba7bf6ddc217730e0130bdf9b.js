44383e0a91678d657c965186e54cfe3c291e3409
Victor Fernandez <xrawvelocity@gmail.com>
2020-11-13 11:31:48 -0500

    first commit

---
+++ build/precache-manifest.c00bac9ba7bf6ddc217730e0130bdf9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15434c57455afb778fc4be6c74fd2c87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24587bfa5293839a2a94",
+    "url": "/static/css/main.7b415d3c.chunk.css"
+  },
+  {
+    "revision": "80308bbbb1f1736ca1fd",
+    "url": "/static/js/2.4b70788c.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.4b70788c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24587bfa5293839a2a94",
+    "url": "/static/js/main.85911118.chunk.js"
+  },
+  {
+    "revision": "1bb895b2429444a647a3",
+    "url": "/static/js/runtime-main.09444656.js"
+  }
+]);
